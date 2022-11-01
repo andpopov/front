3fd10edb33b396b3385a9e3daf57b54897b93d0e
Andrey Popov <andpopov@gmail.com>
2022-11-01 14:56:12 +0300

    1

---
+++ src/main/java/com/artipie/front/rest/SettingsService.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2022 artipie.com
+ * https://github.com/artipie/front/LICENSE.txt
+ */
+package com.artipie.front.rest;
+
+import com.artipie.front.Layout;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+
+/**
+ * Settings-service.
+ *
+ * @since 1.0
+ */
+public class SettingsService extends BaseService {
+    /**
+     * Path to settings root of rest-api.
+     */
+    private static final String SETTINGS_PATH = "/api/v1/settings";
+
+    /**
+     * Path to layout rest-api.
+     */
+    private static final String LAYOUT_PATH = String.format("%s/layout", SETTINGS_PATH);
+
+    /**
+     * Ctor.
+     * @param rest Artipie rest endpoint.
+     */
+    public SettingsService(final String rest) {
+        super(rest);
+    }
+
+    /**
+     * Obtain list of repository names.
+     * @param token Token.
+     * @return List of repository names.
+     */
+    public Layout layout(final String token) {
+        return BaseService.handle(
+            this.httpGet(Optional.of(token), SettingsService.LAYOUT_PATH),
+            res -> {
+                final JsonObject json = BaseService.jsonObject(res);
+                return Layout.valueOf(json.getString("layout"));
+            }
+        );
+    }
+}
